756327a2aba83bca638e3b65a836a7a0aea548d8
max-ra <maxi1221@gmail.com>
2020-10-18 13:43:37 +0200

    integrate changes for OIDC

---
+++ humhub/protected/vendor/worteks/humhub-auth-oidc/src/OIDC.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * @link https://www.humhub.org/
+ * @copyright Copyright (c) 2016 HumHub GmbH & Co. KG
+ * @license https://www.humhub.com/licences
+*/
+
+namespace worteks\humhub\authclient;
+
+use humhub\modules\user\authclient\interfaces\ApprovalBypass;
+use humhub\modules\user\authclient\interfaces\SyncAttributes;
+
+/**
+ * OIDC authentication client
+ *
+ * @since 0.1
+ */
+class OIDC extends \worteks\yii\authclient\OIDC implements ApprovalBypass, SyncAttributes
+{
+    public $cssIcon = 'fa fa-sign-in';
+
+    /**
+     * @inheritdoc
+     */
+    protected function defaultViewOptions()
+    {
+      return [
+        'cssIcon' => $this->cssIcon,
+        'buttonBackgroundColor' => '#4078C0',
+      ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function defaultNormalizeUserAttributeMap()
+    {
+      return [
+        'id' => 'sub',
+        'username' => 'sub',
+        'firstname' => 'given_name',
+        'lastname' => 'family_name'
+      ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getSyncAttributes()
+    {
+        return ["firstname","lastname","email","username"];
+    }
+}
